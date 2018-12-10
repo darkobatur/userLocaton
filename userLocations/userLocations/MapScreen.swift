a8a7e6f5489ffb1da4e46b81b6236633fcd67998
Darko Batur <darkobatur@gmail.com>
2018-12-10 22:27:45 +0100

    userLocationSetup

---
+++ userLocations/userLocations/MapScreen.swift
@@ -0,0 +1,45 @@
+//
+//  ViewController.swift
+//  userLocations
+//
+//  Created by O on 12/6/18.
+//  Copyright © 2018 Darko Batur. All rights reserved.
+//
+
+import UIKit
+import MapKit
+import CoreLocation
+
+class MapScreen: UIViewController {
+    @IBOutlet weak var mapView: MKMapView!
+    
+    let locationManager = CLLocationManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    func setupLocation() {
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+    }
+    
+    func checkLocationServise() {
+        if CLLocationManager.locationServicesEnabled() {
+            setupLocation()
+        } else {
+            // on location servise
+        }
+    }
+
+}
+extension MapScreen: CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        // Locatin
+    }
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        // authorization
+    }
+    
+}
